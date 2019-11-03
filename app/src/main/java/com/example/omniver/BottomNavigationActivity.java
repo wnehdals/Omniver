b209b114215fd0e014515eaf86002eeeb47f9d3e
dongmin960903 <dongmin960903@gmail.com>
2019-11-04 02:06:05 +0900

    initialization

---
+++ app/src/main/java/com/example/omniver/BottomNavigationActivity.java
@@ -0,0 +1,56 @@
+package com.example.omniver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        /*
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+        */
+
+    }
+    protected class BottomNavListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_menu1:
+                    Intent intent1 = new Intent(getApplicationContext(), CategoryActivity.class);
+                    startActivity(intent1);
+                    return true;
+                case R.id.navigation_menu2:
+                    Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent2);
+                    return true;
+                case R.id.navigation_menu3:
+                    Intent intent3 = new Intent(getApplicationContext(), MyInfoActivity.class);
+                    startActivity(intent3);
+                    return true;
+            }
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/example/omniver/CategoryActivity.java
+++ app/src/main/java/com/example/omniver/CategoryActivity.java
@@ -1,25 +1,20 @@
 package com.example.omniver;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-public class MainActivity extends BottomNavigationActivity {
+public class CategoryActivity extends BottomNavigationActivity {
     private BottomNavListener bottomNavListener;
     private BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_category);
         bottomNavListener = new BottomNavListener();
         navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(bottomNavListener);
     }
-
 }
