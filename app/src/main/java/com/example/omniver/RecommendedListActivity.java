746ab7bf2870edcd556011265f3d839c38d7c9c8
YJ CHO <reverince@gmail.com>
2019-12-15 15:29:08 +0900

    Update RecommendedList layout

--- app/src/main/java/com/example/omniver/RecommendedListActivity.java
+++ app/src/main/java/com/example/omniver/RecommendedListActivity.java
@@ -12,14 +12,14 @@ public class RecommendedListActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
 
     final int[][] icons = {
-            {R.mipmap.sleeveless, R.mipmap.shorts, R.mipmap.skirt},
-            {R.mipmap.shorts, R.mipmap.casual_t_shirt_, R.mipmap.long_sleeves_t_shirt},
-            {R.mipmap.long_sleeves_t_shirt, R.mipmap.hoodie, R.mipmap.long_pants},
-            {R.mipmap.neat, R.mipmap.hoodie, R.mipmap.long_pants},
-            {R.mipmap.black_jacket, R.mipmap.long_sleeves_t_shirt, R.mipmap.blouse_with_lace},
-            {R.mipmap.coat, R.mipmap.blouse_with_lace, R.mipmap.scarf},
-            {R.mipmap.coat, R.mipmap.black_jacket, R.mipmap.scarf},
-            {R.mipmap.winter, R.mipmap.black_jacket, R.mipmap.coat}
+            {R.mipmap.sleeveless, R.mipmap.shorts},
+            {R.mipmap.shorts, R.mipmap.casual_t_shirt_},
+            {R.mipmap.long_sleeves_t_shirt, R.mipmap.hoodie},
+            {R.mipmap.neat, R.mipmap.hoodie},
+            {R.mipmap.black_jacket, R.mipmap.long_sleeves_t_shirt},
+            {R.mipmap.coat, R.mipmap.blouse_with_lace},
+            {R.mipmap.coat, R.mipmap.scarf},
+            {R.mipmap.winter, R.mipmap.black_jacket}
     };
     final String[] texts = {
             "나시티, 반바지, 민소매 원피스",
@@ -31,8 +31,8 @@ public class RecommendedListActivity extends AppCompatActivity {
             "코트, 가죽자켓",
             "겨울 옷(야상, 패딩, 목도리 등)"
     };
-    final String[] temperatures1 = { "27", "23", "20", "17", "12", "10", "6", "0"};
-    final String[] temperatures2 = { "99", "26", "22", "19", "16", "11", "9", "5"};
+    final String[] temperatures1 = {"27", "23", "20", "17", "12", "10", "6", "0"};
+    final String[] temperatures2 = {"99", "26", "22", "19", "16", "11", "9", "5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
